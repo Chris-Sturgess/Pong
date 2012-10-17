838de52d14858b3be5317cc343e352f4caf52db9
Christopher Sturgess <Chris@Cassandra.local>
2012-10-16 23:34:05 -0400

    Updated files to display two paddles, and added config file

---
+++ conf.lua
@@ -0,0 +1,5 @@
+function love.conf(t)
+	t.title = "Pong: A Test by Christopher Sturgess"
+	t.screen.width = 1000
+	t.screen.height = 500
+end
--- main.lua
+++ main.lua
@@ -1,13 +1,11 @@
-Pong: A LOVE2D game by Christopher Sturgess
-
+--Pong: A LOVE2D game by Christopher Sturgess
 
 function love.load()
-
-
-
+	
+	player = {x = 30, y = 200, width = 10, height = 100}
+	enemy = {x = 970, y = 200, width = 10, height = 100}
 
 end
-
 
 function love.update()
 
@@ -16,12 +14,11 @@ function love.update()
 
 end
 
-
 function love.draw()
 
-
-
+	love.graphics.setColor(255,255,255)
+	love.graphics.rectangle("fill", player.x, player.y, player.width, player.height)
+	love.graphics.rectangle("fill", enemy.x, enemy.y, enemy.width, enemy.height)
 
 end
-
 
