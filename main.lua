8187294da3a9c42bc80bfd0bd3d670a3a9fc048c
Christopher Sturgess <Chris@Cassandra.local>
2012-10-19 21:47:33 -0400

    Added in scores, a cool font, and split up the functions to make them clearer

--- main.lua
+++ main.lua
@@ -5,12 +5,17 @@ enemySpeed = 5
 ballX = 500
 ballY = 250
 ballSpeed = 5
+playerScore = 0
+enemyScore = 0
+playerTextLoc = 0
 function love.load()
 	
 	player = {x = 30, y = 200, width = 10, height = 100}
 	enemy = {x = 970, y = 200, width = 10, height = 100}
 	ballQuad = love.graphics.newQuad(0, 0, 100, 100, 100, 100) 
 	ball = love.graphics.newImage("Images/Ball.png")
+	scoreFont = love.graphics.setNewFont("fonts/segment14.ttf",100)
+	
 end
 
 function love.update()
@@ -33,17 +38,35 @@ function love.update()
 
 	if ballX >= 980 then 
 		ballSpeed = -5
+		playerScore = playerScore + 1
+		if playerScore == 10 then playerTextLoc = 90 end
 	elseif ballX <= 0 then 
 		ballSpeed = 5 
+		enemyScore = enemyScore + 1
 	end
 
 end
 
 function love.draw()
-
-	love.graphics.setColor(255,255,255)
-	love.graphics.rectangle("fill", player.x, player.y, player.width, player.height)
-	love.graphics.rectangle("fill", enemy.x, enemy.y, enemy.width, enemy.height)
+	drawMidLine()
+	drawPaddles()
+	drawScores()
 	love.graphics.drawq(ball, ballQuad, ballX, ballY, 0, .15, .15)
 end
 
+function drawScores()
+	love.graphics.setFont(scoreFont)
+	love.graphics.print(playerScore, 400 - playerTextLoc, 20, 0)
+	love.graphics.print(enemyScore, 520, 20, 0)
+end
+
+function drawPaddles()
+	love.graphics.setColor(255,255,255)
+	love.graphics.rectangle("fill", player.x, player.y, player.width, player.height)
+	love.graphics.rectangle("fill", enemy.x, enemy.y, enemy.width, enemy.height)
+end
+
+function drawMidLine()
+	love.graphics.setLine(20, "rough")
+	love.graphics.line(490, 0, 490, 500)
+end
