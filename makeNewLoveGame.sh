593df003618f7d58e2fe515a81fa01fbf0191143
Christopher Sturgess <christopher.sturgess@well.ca>
2012-10-24 12:15:30 -0400

    Updated script

--- makeNewLoveGame.sh
+++ makeNewLoveGame.sh
@@ -2,5 +2,13 @@
 
 mkdir $1
 cd $1
-echo $1": A LOVE2D game by Christopher Sturgess\n\n\nfunction love.load()\n\n\n\n\nend\n\n\nfunction love.update()\n\n\n\n\nend\n\n\nfunction love.draw()\n\n\n\n\nend\n\n" > main.lua
+echo "--"$1": A LOVE2D game by Christopher Sturgess\n\n\nfunction love.load()\n\n\n\n\nend\n\n\nfunction love.update()\n\n\n\n\nend\n\n\nfunction love.draw()\n\n\n\n\nend\n\n" > main.lua
+mkdir Resources
+cd Resources
+mkdir Images
+mkdir Audio
+mkdir Fonts
+cd ..
+mkdir Modules
+echo "#"$1 > README.md
 cd ..
